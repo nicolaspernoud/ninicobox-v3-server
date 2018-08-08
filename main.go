ab7882aed76ad28b5167e37738d5cc2f22779649
Nicolas Pernoud <npernoud@grandlyon.com>
2018-08-08 22:18:46 +0200

    Corrected security headers

--- main.go
+++ main.go
@@ -170,8 +170,7 @@ func corsMiddleware(next http.Handler) http.Handler {
 func webSecurityMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Strict-Transport-Security", "max-age=63072000")
-		w.Header().Set("X-Content-Type-Options", "nosniff")
-		w.Header().Set("Content-Security-Policy", " default-src https: 'self' 'unsafe-eval' 'unsafe-inline'; object-src 'none'; frame-ancestors https://*.ninico.fr")
+		w.Header().Set("Content-Security-Policy", "default-src 'self' https: 'unsafe-eval' 'unsafe-inline'; object-src 'none'; frame-ancestors https://*.ninico.fr")
 		w.Header().Set("X-Frame-Options", "DENY")
 		w.Header().Set("X-XSS-Protection", "1; mode=block")
 		w.Header().Set("Referrer-Policy", "same-origin")
