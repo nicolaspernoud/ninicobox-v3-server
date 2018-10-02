d4d1936d4613b9de0d8af0cd8df4d5a2fcf21976
Nicolas Pernoud <nper.public@gmail.com>
2018-10-02 21:42:22 +0200

    Updated version

--- types/types.go
+++ types/types.go
@@ -176,7 +176,7 @@ func InfosFromJSONFiles() (Infos, error) {
 		return Infos{}, err
 	}
 	return Infos{
-		ServerVersion: "3.0.3",
+		ServerVersion: "3.0.4",
 		ClientVersion: clientVersion,
 		Bookmarks:     bookmarks,
 	}, nil
