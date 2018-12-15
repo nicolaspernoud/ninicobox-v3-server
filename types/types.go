60395473abec7412e4281abf8d806443f3274247
Nicolas Pernoud <nper.public@gmail.com>
2018-12-15 14:21:59 +0100

    Refactoring

--- types/types.go
+++ types/types.go
@@ -201,7 +201,7 @@ func InfosFromJSONFiles() (Infos, error) {
 		return Infos{}, err
 	}
 	return Infos{
-		ServerVersion: "3.0.16",
+		ServerVersion: "3.0.17",
 		ClientVersion: clientVersion,
 		Bookmarks:     bookmarks,
 	}, nil
